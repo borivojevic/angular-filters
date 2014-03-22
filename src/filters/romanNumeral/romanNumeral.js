71c5e7a0526930b5f8531d17a63d4211148170b0
Mirko Borivojevic <mirko@mirkoborivojevic.com>
2014-03-22 16:23:41 +0100

    arabic to roman numeral conversion function

--- src/filters/romanNumeral/romanNumeral.js
+++ src/filters/romanNumeral/romanNumeral.js
@@ -1,6 +1,24 @@
 angular.module('borivojevic.filters', []).
   filter('romanNumeral', function() {
-    return function(input) {
-      return "";
-    };
+    return function(number) {
+      var result = '';
+      var arabic = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+      var roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+      if (number < 1) {
+        return result;
+      }
+
+      while (number > 0) {
+        arabic.forEach(function(div, i) {
+          var res = Math.floor(number / div);
+          if (res >= 1) {
+            result = result + Array(res+1).join(roman[i]);
+            number = number % div;
+          }
+        });
+      }
+
+      return result;
+    }
   });
--- test/romanNumeral/romanNumeralSpec.js
+++ test/romanNumeral/romanNumeralSpec.js
@@ -11,4 +11,35 @@ describe('Converting arabic numbers to roma numerals', function() {
   it('should return empty string for 0', function() {
     expect(romanNumeralFilter(0)).toEqual('');
   });
+
+  it('should return convert arabic into roman number', function() {
+    var conversions = {
+      1: 'I',
+      2: 'II',
+      4: 'IV',
+      5: 'V',
+      6: 'VI',
+      9: 'IX',
+      10: 'X',
+      40: 'XL',
+      50: 'L',
+      60: 'LX',
+      90: 'XC',
+      100: 'C',
+      110: 'CX',
+      400: 'CD',
+      500: 'D',
+      600: 'DC',
+      900: 'CM',
+      1000: 'M',
+      1100: 'MC',
+      3497: 'MMMCDXCVII',
+      1987: 'MCMLXXXVII'
+    };
+    Object.keys(conversions).forEach(function(arabic) {
+      var roman = romanNumeralFilter(arabic);
+      expect(roman).toEqual(conversions[arabic]);
+    });
+  });
+
 });
